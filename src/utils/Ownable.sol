305ae6bd36d3cec48dec42eaaea8fc194076d4f9
Ian Elliott <ie2173@columbia.edu>
2023-07-25 14:44:08 -0700

    added tests

---
+++ src/utils/Ownable.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.20; 
+
+interface IOwnable {
+    /// @dev Returns the owner of the contract.
+    function owner() external view returns (address);
+
+    /// @dev Lets a module admin set a new owner for the contract. The new owner must be a module admin.
+    function setOwner(address _newOwner) external;
+
+    /// @dev Emitted when a new Owner is set.
+    event OwnerUpdated(address indexed prevOwner, address indexed newOwner);
+}
+
+    
+abstract contract Ownable is IOwnable {
+    /// @dev Owner of the contract (purpose: OpenSea compatibility)
+    address internal  _owner;
+
+    /// @dev Reverts if caller is not the owner.
+    modifier onlyOwner() {
+        if (msg.sender != _owner) {
+            revert("Not authorized");
+        }
+        _;
+    }
+
+    /**
+     *  @notice Returns the owner of the contract.
+     */
+    function owner() public view override returns (address) {
+        return _owner;
+    }
+
+    /**
+     *  @notice Lets an authorized wallet set a new owner for the contract.
+     *  @param _newOwner The address to set as the new owner of the contract.
+     */
+    function setOwner(address _newOwner) onlyOwner external override {
+        _setupOwner(_newOwner);
+    }
+
+    /// @dev Lets a contract admin set a new owner for the contract. The new owner must be a contract admin.
+    function _setupOwner(address _newOwner) internal {
+        address _prevOwner = _owner;
+        _owner = _newOwner;
+
+        emit OwnerUpdated(_prevOwner, _newOwner);
+    }
+
+   
+
+}
